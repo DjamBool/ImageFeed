3c5e1da71e6d5330b87b4868c486bf0697983042
JamBool <mailforgit2023@gmail.com>
2023-08-28 21:11:19 +0300

    В Assets добавлены цвета и картинка Vector. Настроен  LaunchScreen

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+let blackColor = UIColor(named: "YP Black")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = blackColor
+        
     }
 
 
