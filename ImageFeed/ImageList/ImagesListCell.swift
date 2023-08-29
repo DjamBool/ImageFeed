5dc490a67131fd7099153a31e2735996e78c414c
JamBool <mailforgit2023@gmail.com>
2023-08-29 16:48:57 +0300

    Добавлен файл ImagesListCell, в нем - идентификатор reuseIdentifier

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Игорь Мунгалов on 29.08.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+}
