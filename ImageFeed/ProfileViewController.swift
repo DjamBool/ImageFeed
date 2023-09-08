dcd1432dfd0d704c73f3e00d38529468b0ae0983
JamBool <mailforgit2023@gmail.com>
2023-09-08 10:14:27 +0300

    в классе ProfileViewController созданы @IBOutlet'ы для фото и лэйблов профиля, и @IBAction для кнопки

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var profileEmailLabel: UILabel!
+    @IBOutlet weak var profileStatusLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
        
     }
-
+    @IBAction func exitButton(_ sender: Any) {
+    }
+    
 }
