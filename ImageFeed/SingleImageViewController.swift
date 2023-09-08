0ca012efd0c3c6282de59f7c8c0f14486eb9e0f3
JamBool <mailforgit2023@gmail.com>
2023-09-08 14:38:52 +0300

    Создан контроллер для показа одной картинки. Сверстан экран для показа одного изображения

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Игорь Мунгалов on 08.09.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
