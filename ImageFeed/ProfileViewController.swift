c8672d5e94642325a5a67b007075905609077de5
JamBool <mailforgit2023@gmail.com>
2023-09-08 09:12:42 +0300

    в Assets картинки like_button_off и like_button_on перемещены в папку LikeButtons. Создан класс ProfileViewController. Создан новый UIViewController в Main.storyobard и связан с классом ProfileViewController

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Игорь Мунгалов on 08.09.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
