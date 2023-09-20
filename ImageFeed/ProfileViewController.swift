3a33151d4c8a2da76010216b079c5841f161bb37
JamBool <mailforgit2023@gmail.com>
2023-09-20 10:44:46 +0300

    добавлена и размещена кнопка logoutButton. При нажатии - удаляется содержимое лэйблов

--- ImageFeed/ProfileViewController.swift
+++ ImageFeed/ProfileViewController.swift
@@ -8,16 +8,18 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     let profileImage = UIImage(named: "Userpick")
     let profileImageView = UIImageView()
+    var logoutButton = UIButton()
     let nameLabel = UILabel()
-    let eMailLabel = UILabel()
-    let statusLabel = UILabel()
+    let loginNameLabel = UILabel()
+    let descriptionLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         makeProfileImage()
+        makeExitButton()
         makeNameLabel()
         makeEMailLabel()
         makeStatusLabel()
@@ -30,51 +32,78 @@ class ProfileViewController: UIViewController {
         view.addSubview(profileImageView)
     }
     
+    func makeExitButton() {
+       logoutButton = UIButton.systemButton(
+        with: (UIImage(named: "Exit"))!,
+            target: self,
+            action: #selector(Self.didTapButton))
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.tintColor = UIColor(named: "YP Red")
+        view.addSubview(logoutButton)
+    }
+    
+    @objc func didTapButton() {
+        print(#function)
+        profileImageView.image = UIImage(systemName: "person.crop.circle.fill")
+        profileImageView.tintColor = .gray
+        nameLabel.text = ""
+        descriptionLabel.text = ""
+        loginNameLabel.text = ""
+    }
+    
     func makeNameLabel() {
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.text = "Екатерина Новикова"
         nameLabel.textColor = UIColor(named: "YP White") ?? UIColor.white
-        nameLabel.font = UIFont(name: "SF Pro" , size: 23)
-        nameLabel.font = UIFont.boldSystemFont(ofSize: 23)
+        nameLabel.font = UIFont(name: "HelveticaNeue-Bold" , size: 23)
         view.addSubview(nameLabel)
     }
     func makeEMailLabel() {
-        eMailLabel.translatesAutoresizingMaskIntoConstraints = false
-        eMailLabel.text = "@ekaterina_nov"
-        eMailLabel.textColor = UIColor(named: "YP Gray") ?? UIColor.systemGray4
-        eMailLabel.font = UIFont(name: "SF Pro" , size: 13)
-        view.addSubview(eMailLabel)
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.textColor = UIColor(named: "YP Gray") ?? UIColor.systemGray4
+        loginNameLabel.font = UIFont(name: "SF Pro" , size: 13)
+        view.addSubview(loginNameLabel)
     }
     func makeStatusLabel() {
-        statusLabel.translatesAutoresizingMaskIntoConstraints = false
-        statusLabel.text = "Hello, world!"
-        statusLabel.textColor = UIColor(named: "YP White") ?? UIColor.white
-        statusLabel.font = UIFont(name: "SF Pro" , size: 13)
-        view.addSubview(statusLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = UIColor(named: "YP White") ?? UIColor.white
+        descriptionLabel.font = UIFont(name: "SF Pro" , size: 13)
+        view.addSubview(descriptionLabel)
     }
     
     func layout() {
         NSLayoutConstraint.activate([
-            
+            // profileImageView
             profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 52),
             profileImageView.heightAnchor.constraint(equalToConstant: 70),
             profileImageView.widthAnchor.constraint(equalToConstant: 70),
             
+           // logoutButton
+            logoutButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 76),
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            
+            // nameLabel
             nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
             nameLabel.heightAnchor.constraint(equalToConstant: 25),
             nameLabel.widthAnchor.constraint(equalToConstant: 241),
             
-            eMailLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            eMailLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
-            eMailLabel.heightAnchor.constraint(equalToConstant: 18),
-            eMailLabel.widthAnchor.constraint(equalToConstant: 241),
-
-            statusLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            statusLabel.topAnchor.constraint(equalTo: eMailLabel.bottomAnchor, constant: 8),
-            statusLabel.heightAnchor.constraint(equalToConstant: 18),
-            statusLabel.widthAnchor.constraint(equalToConstant: 241)
+            //loginNameLabel
+            loginNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginNameLabel.heightAnchor.constraint(equalToConstant: 18),
+            loginNameLabel.widthAnchor.constraint(equalToConstant: 241),
+            
+            //descriptionLabel
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 18),
+            descriptionLabel.widthAnchor.constraint(equalToConstant: 241)
         ])
     }
 }
