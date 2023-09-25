46dff87bb52a239500ecec978625e8c6f9b6879b
JamBool <mailforgit2023@gmail.com>
2023-09-25 13:14:27 +0300

    добавлен View Controller, назначен цвет фона. На View добавлен Image View: добавлены констрейнты. На View добавлена кнопка, добавлены констрейнты соответствующие макету: Width Fixed (288px), Height Fixed (48px). Создан class AuthViewController

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Игорь Мунгалов on 22.09.2023.
+//
+
+import Foundation
+
+let accessKey = "C7KQQn0EnC0wMO2JkCbuThc7rjLpxxuM49mgg-GSPXQ"
+let secretKey = "YZGi5nJD7kvm7UIs_Fa8F3h4KnbFPUtyy1qDiO5g8G8"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
