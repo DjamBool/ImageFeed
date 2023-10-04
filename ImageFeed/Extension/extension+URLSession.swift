3004df93f8bfffee715097eea347874abb4c40cf
JamBool <mailforgit2023@gmail.com>
2023-10-04 16:22:34 +0300

    В OAuth2Service добавлены поля; добавлены расширения для URLRequest и URLSession

---
+++ ImageFeed/Extension/extension+URLSession.swift
@@ -0,0 +1,37 @@
+
+import Foundation
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
+}
+
+extension URLSession {
+    func data(
+        for request: URLRequest, completion: @escaping (Result<Data, Error>) -> Void) -> URLSessionTask {
+        let fulfillCompletion: (Result<Data, Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+        let task = dataTask(with: request, completionHandler: { data, response, error in
+            if let data = data,
+               let response = response,
+               let statusCode = (response as? HTTPURLResponse)?.statusCode
+            {
+                if statusCode > 200 || statusCode < 300 {
+                    fulfillCompletion(.success(data))
+                } else {
+                    fulfillCompletion(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error = error {
+                fulfillCompletion(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                fulfillCompletion(.failure(NetworkError.urlSessionError))
+            }
+        })
+        task.resume()
+        return task
+    }
+}
--- ImageFeed/OAuth/OAuth2Service.swift
+++ ImageFeed/OAuth/OAuth2Service.swift
@@ -9,6 +9,17 @@ import Foundation
 
 class OAuth2Service {
    
+    static let shared = OAuth2Service()
+    private let urlSession = URLSession.shared
+    private (set) var authToken: String? {
+        get {
+            return OAuth2TokenStorage().token
+        }
+        set {
+            OAuth2TokenStorage().token = newValue
+        }
+    }
+    
     private func fetchAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
         
     }
