2d3732f24065fec23392e8a680f2e233cc7db0b5
JamBool <mailforgit2023@gmail.com>
2023-10-04 13:41:06 +0300

    Создан новый класс OAuth2Service,  в нем объявлена функция fetchAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void)

---
+++ ImageFeed/OAuth/OAuth2Service.swift
@@ -0,0 +1,16 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//  Created by Игорь Мунгалов on 04.10.2023.
+//
+
+import Foundation
+
+class OAuth2Service {
+   
+    private func fetchAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        
+    }
+}
+
