b7a5325b2fc8c2aa11ded50837ddc55f9ec918e8
JamBool <mailforgit2023@gmail.com>
2023-10-16 12:13:35 +0300

    замены обращения к ProgressHUD на обращения к UIBlockingProgressHUD

--- ImageFeed/SplashViewController.swift
+++ ImageFeed/SplashViewController.swift
@@ -50,7 +50,8 @@ extension SplashViewController {
 
 extension SplashViewController: AuthViewControllerDelegate {
     func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.show()
+        //ProgressHUD.show()
+        UIBlockingProgressHUD.show()
         dismiss(animated: true) { [weak self] in
             guard let self = self else { return }
             self.fetchOAuthToken(code)
@@ -63,9 +64,11 @@ extension SplashViewController: AuthViewControllerDelegate {
             switch result {
             case .success:
                 self.switchToTabBarController()
-                ProgressHUD.dismiss()
+                //ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
             case .failure:
-                ProgressHUD.dismiss()
+               // ProgressHUD.dismiss()
+                UIBlockingProgressHUD.dismiss()
                 // TODO [Sprint 11]
                 break
             }
